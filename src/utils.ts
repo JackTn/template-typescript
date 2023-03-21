94f33748cde22ca5fd96524a54d517e6e5299431
v-tianxi <v-tianxi@microsoft.com>
2023-03-21 16:30:59 +0800

    add utils class

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,31 @@
-import { repo, organization } from "./Constants"
+import { repo, organization } from './Constants';
 
-console.log(repo)
-console.log(organization)
+console.log(repo);
+console.log(organization);
+
+class C {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class A {
+  protected num: number;
+
+  constructor(num: number) {
+    this.num = num;
+  }
+}
+
+class X extends A {
+  protected name: string;
+
+  constructor(num: number, name: string) {
+    super(num);
+    this.name = name;
+  }
+}
