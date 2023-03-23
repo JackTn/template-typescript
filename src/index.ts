0600081f5c63387f5b528376cdb03388701565b3
v-tianxi <v-tianxi@microsoft.com>
2023-03-23 16:35:38 +0800

    fix vscode bug

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 console.log(`Hello Node.js v${process.versions.node}!`);
-console.log(`Write on ${process.cwd()}`)
+console.log(`Write on ${process.cwd()}`);
 
-
-import { repo, organization } from "./Constants"
-console.log(repo)
-console.log(organization)
+import { repo, organization } from './Constants';
+console.log(repo);
+console.log(organization);
 
 const a = async () => {
-    setTimeout(()=> console.log(123), 3000)
+  setTimeout(() => console.log(123), 3000);
+};
+
+export function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 const b = async () => {
-    await a()
-    await a()
-    await a()
-    await a()
-    await a()
+  await a();
+  await delay(5000);
+  await a();
+  await delay(5000);
+  await a();
 };
 b();
